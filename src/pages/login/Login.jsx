cf019ee93d924f15cfaf04de6879d4fa466812f1
Jaime <72435300+jsr-desarrolloweb@users.noreply.github.com>
2021-05-18 13:12:40 +0200

    Setting some login errors

--- src/pages/login/Login.jsx
+++ src/pages/login/Login.jsx
@@ -5,18 +5,25 @@ import {loginUser, useAuthState, useAuthDispatch} from '../../context'
 const Login = (props) => {
     const [username, setUsername] = useState('')
     const [password, setPassword] = useState('')
+    const [loginError, setLoginError] = useState(null)
 
     const dispatch = useAuthDispatch()
     const {loading, errorMessage} = useAuthState()
 
     const handleLogin = async (e) => {
         e.preventDefault()
+        if (!username.trim() || !password.trim()) {
+            setLoginError('Fill the inputs')
+            return
+        }
         let payload = {username, password}
         // async request to the server
         try {
             let response = await loginUser(dispatch, payload) //loginUser action makes the request and handles all the neccessary state changes
             // console.log(response)
             if (!response.user) return
+            setUsername('')
+            setPassword('')
             props.history.push('/dashboard') //navigate to dashboard on success
         } catch (error) {
             console.log(error)
@@ -29,6 +36,9 @@ const Login = (props) => {
             <p>This is the login form page</p>
             {
                 errorMessage ? <p>{errorMessage}</p> : null
+            }
+            {
+                loginError ? <p>{loginError}</p> : null
             }
             <form>
                 <div>
